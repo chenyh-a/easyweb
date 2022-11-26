dc5fda48be151692b0e2d6f339394c6e92761409
chenyh-a <343271084@qq.com>
2022-11-26 22:23:42 +0800

    change java

--- kftest/src/main/java/com/example/kafka/OrderConsumer.java
+++ kftest/src/main/java/com/example/kafka/OrderConsumer.java
@@ -1,22 +1,18 @@
 package com.example.kafka;
 
-import org.slf4j.Logger;
-import org.slf4j.LoggerFactory;
-import org.springframework.kafka.annotation.KafkaListener;
 import org.springframework.stereotype.Component;
+import org.springframework.kafka.annotation.KafkaListener;
 
 /**
  * @author chenyh-a
- * @version created: 2022-11-26 12:00
+ * @version created: 2022-11-26 20:50
  * 
  */
 @Component
 public class OrderConsumer {
-	private static Logger log = LoggerFactory.getLogger(OrderConsumer.class);
-
 	@KafkaListener(topics = { "TOPIC_ORDER" }, groupId = "GROUP_LOGISTICS")
-	public void consume(String message) {
-		// put actual consumer code here.
-		log.info("Consumer receive: " + message);
+	public void onMessage(String message) {
+		System.out.println("Order consumer received:" + message);
+		//write actual consumer code here
 	}
 }
--- kftest/src/main/java/com/example/kafka/OrderController.java
+++ kftest/src/main/java/com/example/kafka/OrderController.java
@@ -2,8 +2,6 @@ package com.example.kafka;
 
 import java.util.Map;
 
-import org.slf4j.Logger;
-import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.kafka.core.KafkaTemplate;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -11,29 +9,20 @@ import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 /**
- * Order producer sample
  * @author chenyh-a
- * @version created: 2022-11-26 11:52
+ * @version created: 2022-11-26 20:43
  * 
  */
-
 @RestController
 public class OrderController {
-	private static Logger log = LoggerFactory.getLogger(OrderController.class);
-
 	@Autowired
-	private KafkaTemplate<String, String> kafkaTemplate;
+	private KafkaTemplate<String, String> kt;
 
 	@RequestMapping(value = "/send")
-	public String sendOrder(@RequestParam Map<String, String> params) {
-		
-		// change it to data collected from front-end
-		String sorder = "{\"orderId\":1234,\"memberId\":\"2345\",\"productId\":\"3456\"}";
-		
-		kafkaTemplate.send("TOPIC_ORDER", "key", sorder);
-		
-		log.debug("Kafka send order success.");
-		
+	public String send(@RequestParam Map<String, String> params) {
+		// change it to actual code, collect front-end data,etc.
+		String sorder = "{\"orderId\":12345}";
+		kt.send("TOPIC_ORDER", "key", sorder);
 		return "{\"result\":\"SUCCESS\"}";
 	}
 
