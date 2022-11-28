af9bd88b250a45a04c84ccf9772a765bcc5c16c5
chenyh-a <343271084@qq.com>
2022-11-28 19:46:48 +0800

    add remarks

--- kftest/src/main/java/com/example/kafka/KftestApplication.java
+++ kftest/src/main/java/com/example/kafka/KftestApplication.java
@@ -3,6 +3,11 @@ package com.example.kafka;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+/**
+ * @author chenyh-a
+ * @version created: 2022-11-28 15:51
+ * 
+ */
 @SpringBootApplication
 public class KftestApplication {
 
--- kftest/src/main/java/com/example/kafka/ServletInitializer.java
+++ kftest/src/main/java/com/example/kafka/ServletInitializer.java
@@ -3,6 +3,11 @@ package com.example.kafka;
 import org.springframework.boot.builder.SpringApplicationBuilder;
 import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
 
+/**
+ * @author chenyh-a
+ * @version created: 2022-11-28 15:51
+ * 
+ */
 public class ServletInitializer extends SpringBootServletInitializer {
 
 	@Override
